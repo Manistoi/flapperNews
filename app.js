bd7d877c4da206cbd5d508d4f5976cf717415790
Manuel Stoilov <mstoi002@fiu.edu>
2016-09-12 15:37:56 +0000

    Began routing process. Creating comments link

--- app.js
+++ app.js
@@ -1,25 +1,52 @@
-var app = angular.module('flapperNews', []);
+var app = angular.module('flapperNews', ['ui.router']); //create angular module named 'flapperNews' that has a ui.router dependency
+app.config([  // use the module's config function to create routes
+'$stateProvider',  //used to watch for the state of apps - A state being a "place" in the app in terms of the overall UI and navigation.
+'$urlRouterProvider', //watches for the $location
+  function($stateProvider, $urlRouterProvider){
+    
+    $stateProvider
+      .state ('home', {
+        url: "/home",
+        templateUrl: "/home.html",
+        controller: 'MainCtrl'
+      });
+      
+    $stateProvider
+      .state('posts', {
+        url: '/posts/{id}',
+        templateUrl: '/posts.html',
+        controller: 'PostsCtrl'
+      }); 
+      
+      $urlRouterProvider.otherwise('home');
+}]);
 
+app.factory('posts', [function(){
+  var o = {
+    posts: [{title: 'hello post1', link: '', upvotes: 0 }]
+  };
+  return o;
+}])
 
 app.controller('MainCtrl', [
 '$scope',
-function($scope){
+'posts',
+function($scope, posts){
   $scope.test = 'Hello world!';
   
-  $scope.posts = [
-  {title: 'post 1', upvotes: 5},
-  {title: 'post 2', upvotes: 2},
-  {title: 'post 3', upvotes: 15},
-  {title: 'post 4', upvotes: 9},
-  {title: 'post 5', upvotes: 4},
- ];
+  $scope.posts = posts.posts;
  
  $scope.addPost = function(){
     if(!$scope.title || $scope.title === '') { return; }
     $scope.posts.push({
       title: $scope.title,
       link: $scope.link,
-      upvotes: 0});
+      upvotes: 0,
+      comments: [
+        {author: 'Joe', body: 'cool post!', upvotes: 0},
+        {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+        ]
+    });
     $scope.title = '';
     $scope.link = '';
   }
@@ -28,4 +55,17 @@ function($scope){
     post.upvotes += 1;
   }
 }]);
+
+app.controller('PostsCtrl', [
+  '$scope',
+  '$stateParams',
+  function($scope, $stateParams, posts) {
+    $scope.post = posts.posts[$stateParams.id];
+}]);
+
+
+
+
+
+
 
